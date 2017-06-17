4405dab2d9c56e12e2eb14ecd57e4796787503b3
Candace <camauric@yahoo.com>
2017-06-16 21:18:30 -0500

    Update buildMatrix.js

--- js/buildMatrix.js
+++ js/buildMatrix.js
@@ -88,9 +88,3 @@ var buildMatrix = function(){
 return(matrix);
 
 }; //end document.ready()function
-
-var strip = function (html){
-   var tmp = document.implementation.createHTMLDocument("New").body;
-   tmp.innerHTML = html;
-   return tmp.textContent || tmp.innerText || "";
-};
