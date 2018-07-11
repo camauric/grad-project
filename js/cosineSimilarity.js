bc474e0353bf4ed776740f232419b2a390e81226
Candace <camauric@yahoo.com>
2018-07-11 14:07:32 -0400

    Update cosineSimilarity.js

--- js/cosineSimilarity.js
+++ js/cosineSimilarity.js
@@ -14,8 +14,7 @@ var cosineSimilarity = function(inputArray, currentQuestionArray){
 	var similarity = 0;
 	var limitFlag = false;
 	var values = [];
-	//console.log( "INPUT ARRAY :  " + inputArray);
-	//console.log("currentQuestionArray : " + currentQuestionArray);
+	
 	value = getValues(inputArray,currentQuestionArray);
 	var numerator = value[0];
 	var denominator = value[1];
@@ -40,9 +39,6 @@ var cosineSimilarity = function(inputArray, currentQuestionArray){
  */
 var getValues = function(inputArray, currentQuestionArray){
 
-	//console.log("Input Array: " + inputArray);
-	//console.log("Current Question Array : " + currentQuestionArray);
-
 	var sumA = sumB = answer = numerator = denominator = 0;
 
 	for(var i = 0; i < inputArray.length; i++){
@@ -54,10 +50,6 @@ var getValues = function(inputArray, currentQuestionArray){
 	}
 
 	denominator = Math.sqrt(sumA) * Math.sqrt(sumB);
-	//console.log("NUMERATOR SUM : " + numerator);
-	//console.log("DENONMINATOR SUMS " + sumA +" " + sumB + " " + denominator);
-	//answer = [numerator, denominator];
-	//console.log("COSINE ANSWER : " + answer);
 	return [numerator, denominator];
 };
 /**
@@ -74,7 +66,7 @@ var getCosineTotals = function(inputArray, matrix){
 
 			cosineTotals.push(cosineSimilarity(inputArray, matrix[i]));
 	}
-	//console.log("Cosine Totals Array : " + cosineTotals);
+	
 	return cosineTotals;
 
 };
